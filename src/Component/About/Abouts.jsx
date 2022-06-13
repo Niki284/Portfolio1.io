d08c09c237273ce8f585f6a60d85a8afe4885233
Nikita Dymovich <89484300+Niki284@users.noreply.github.com>
2022-06-13 10:57:30 +0200

    Update Abouts.jsx

--- src/Component/About/Abouts.jsx
+++ src/Component/About/Abouts.jsx
@@ -11,16 +11,13 @@ export default function Abouts() {
             <h2>Mijn naam is Nikita
             </h2>
             <p>
-            Ik ben 22 jaar oud. Ik kom uit Wit-Rusland.
-            Mijn hobby’s zijn schaken,  programmeren , lopen  en tennis met vrienden.
-            Mijn studies zijn : 
-            Elektronica-ICT – ICT bij op Technologiecampus Odisee in Gent.
-            Graduaad programmeren bij Campus Mariakerke, Campus .
-            Ik wil graag later werken als front-end als ook back-end  en designer.
-            Ik wil graag problemen analyseren en ze oplossen door de kennis toe te passen die ik heb.6
-            De Technologieën dat ik ken zijn werken met laravel html ,css ,scss js , mysql 
-            een ook met react.
-            Mijn projecten waren Snake game Gentse feesten.
+           Ik ben 22 jaar oud. Ik kom uit Wit-Rusland. 
+    Mijn hobby’s zijn schaken, programmeren , lopen en tennis met vrienden.
+    Mijn studies zijn : Elektronica-ICT – ICT op de Technologiecampus Odisee in Gent en Graduaat programmeren aan de Artevelde Hogeschool in Gent.
+    Ik wil graag later werken als front-end als ook back-end en designer.
+    Ik wil graag problemen analyseren en ze oplossen door de kennis toe te passen die ik heb. 
+    De Technologieën waar ik kennis van heb en mee kan werken zijn laravel, html ,css ,scss, js , 
+    mysql en react. Ik heb al enkele projecten gemaakt waaronder Snake game en Gentse feesten.
 
             </p>
         </div>
