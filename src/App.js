8ca2d2aae112c321201cc015b1a9b7594de49eb7
Nikita Dymovich <89484300+Niki284@users.noreply.github.com>
2025-06-30 23:53:37 +0200

    app.js  de comentaar op import weg gedaan

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-// import './cstyle.css'
 import {Routes, Route, Link, Router} from 'react-router-dom';
 import {Home} from './pages/Home';
 import {About} from './pages/About';
